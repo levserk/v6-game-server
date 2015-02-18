c92f8f8fbadd2901ca198628b76bae6a092538a8
levserk <levserk@gmail.com>
2015-02-18 13:48:36 +0300

    remove game logic in default engine

--- test/conf.js
+++ test/conf.js
@@ -4,7 +4,7 @@ module.exports = {
     pingTimeout:100000,
     pingInterval:10000,
     logLevel:3,
-    turnTime: 10,   // user turn time in seconds
+    turnTime: 20,   // user turn time in seconds
     maxTimeouts: 1, // count user timeouts in game to lose
     ratingElo:true,
     mode: 'debug', // set developing mode, db isn't required

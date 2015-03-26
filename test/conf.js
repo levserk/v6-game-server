0e089aaa14ee7491ca0427e57c205ed1e12d7f58
levserk <levserk@gmail.com>
2015-03-26 18:04:48 +0300

    0.7.3

--- test/conf.js
+++ test/conf.js
@@ -1,21 +1,22 @@
 module.exports = {
-    game: 'test', // required, game name
-    port: 8080,
+    game: 'test2', // required, game name
+    port: 8078,
     pingTimeout:100000,
     pingInterval:10000,
     logLevel:3,
     turnTime: 20,   // user turn time in seconds
     maxTimeouts: 1, // count user timeouts in game to lose
-    ratingElo:true,
+    minTurns: 1,
+    loseOnLeave: true,
     mode: 'debug', // set developing mode, db isn't required
     gameModes: ['mode_1', 'mode_2'], // game modes, with different history, ratings, games, default is one mode ['default']
     modesAlias:{'mode_1':'mode first', 'mode_2': 'mode second'},
+    adminList: ['85505'],
     db:{
         connectionLimit : 4,
         host            : 'localhost',
         user            : 'root',
         password        : 'root',
         database        : 'logicgame'
-    },
-    closeOldConnection: true
+    }
 };
--- test/engine.js
+++ test/engine.js
@@ -1,4 +1,8 @@
 module.exports = {
+    initUserData: function(mode, modeData){
+        if (!modeData.score) modeData.score = 100;
+        return modeData;
+    },
     getGameResult: function(room, user, turn){
         switch (turn.result){
             case 0: // win second player, white
@@ -24,10 +28,40 @@ module.exports = {
         }
         throw new Error('can not compute winner! room:' + room.id + ' result: ' + turn.result);
     },
+    getUsersScores: function(room, result){
+        for (var i = 0; i < room.players.length; i++){
+            if (room.players[i] == result.winner)
+                room.players[i][room.mode].score += 10;
+            else room.players[i][room.mode].score -= 10;
+        }
+        return result;
+    },
     switchPlayer:function(room, user, turn){
         if (turn.switch){
             if (room.players[0] == user) return room.players[1];
             else return room.players[0];
+        }
+        return user;
+    },
+    userEvent: function(room, user, event){
+        return {
+            event: event,
+            target: room
+        }
+    },
+    gameEvent: function(room, user, event, flagRoundStart){
+        if (flagRoundStart){
+            var data = [];
+            for (var i = 0; i < room.players.length; i++) {
+                data.push({
+                    target: room.players[i],
+                    event: {
+                        type: 'startEvent',
+                        data: room.players[i].userId
+                    }
+                });
+            }
+            return data;
         }
     }
 };

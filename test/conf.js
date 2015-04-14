157adfb9fc871df631aeae75ccc00db73e919823
levserk <levserk@gmail.com>
2015-04-14 17:58:33 +0300

    add game take backs option

--- test/conf.js
+++ test/conf.js
@@ -7,6 +7,7 @@ module.exports = {
     turnTime: 30,   // user turn time in seconds
     maxTimeouts: 1, // count user timeouts in game to lose
     minTurns: 0,
+    takeBacks: 1,
     mode: 'debug', // set developing mode, db isn't required
     gameModes: ['mode_1', 'mode_2'], // game modes, with different history, ratings, games, default is one mode ['default']
     modesAlias:{'mode_1':'mode first', 'mode_2': 'mode second'},

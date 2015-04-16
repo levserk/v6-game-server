ddbb5b89658a57899653ca5ceae1215f0f2c3f56
levserk <levserk@gmail.com>
2015-04-16 17:00:26 +0300

    add logger

--- test/conf.js
+++ test/conf.js
@@ -8,7 +8,7 @@ module.exports = {
     maxTimeouts: 1, // count user timeouts in game to lose
     minTurns: 0,
     takeBacks: 1,
-    mode: 'debug', // set developing mode, db isn't required
+    mode: 'develop1', // set developing mode, db isn't required
     gameModes: ['mode_1', 'mode_2'], // game modes, with different history, ratings, games, default is one mode ['default']
     modesAlias:{'mode_1':'mode first', 'mode_2': 'mode second'},
     adminList: ['85505'],
